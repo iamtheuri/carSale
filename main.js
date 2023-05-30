ada9b8f48f3cb85151cc4aaeaa6bac11e96cd75e
iamtheuri <theuridavid56@gmail.com>
2023-05-30 23:44:23 +0300

    Final Commit

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').addEventListener('click', () => {
     search.classList.toggle('active');
+    menu.classList.remove('active');
 });
+
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').addEventListener('click', () => {
+    menu.classList.toggle('active');
+    search.classList.remove('active');
+});
+// Hide Menu & Search Bar On Scroll
+window.onscroll = () => {
+    menu.classList.remove('active');
+    search.classList.remove('active');
+}
 
 // Header
 let header = document.querySelector('header');
